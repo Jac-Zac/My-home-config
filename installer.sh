051cc1545231be201952cff107da85fa1f930054
Jac-Zac <jacopozac@icloud.com>
2020-06-30 19:18:48 +0200

    fix 2
    Former-commit-id: eb9330c67d00d4b614668c4bcf82c1d45a5ea119

--- installer.sh
+++ installer.sh
@@ -4,6 +4,7 @@ brew update && brew upgrade
 echo "brew installed"
 
  # install things to make yuor rice look like mine
+brew install wget
 brew install tty-clock
 brew install gotop
 brew install lsd
--- root_script.sh
+++ root_script.sh
@@ -10,16 +10,19 @@ fi
 # Install ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-
 # download and clean up 
-sudo mv config $HOME/.config
+sudo cp -r config $HOME/.config
 sudo mv ohmyzsh $HOME/.config
 mkdir .cache
 mkdir .cache/zsh
 sudo cp profile $HOME/.profile
 sudo cp zprofile $HOME/.zprofile
 sudo cp -r vim $HOME/.vim
+
+# changing owneriship 
+sudo chown $USER .vim
+sudo chown $USER .profile
+sudo chown $USER .zprofile
 
 # plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
