6e0e218cd6f58e314b4e5578a889ada6e08f6808
Jac-Zac <jacopozac@icloud.com>
2020-06-30 18:58:31 +0200

    fix
    Former-commit-id: c1148ca1444e35411673da82fcad159ed2cace01

--- installer.sh
+++ installer.sh
@@ -2,5 +2,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"i
 brew update && brew upgrade
 echo "brew installed"
+
+ # install things to make yuor rice look like mine
+brew install tty-clock
+brew install gotop
+brew install lsd
+
 chmod +x root_script.sh
 echo "run next script as root"
