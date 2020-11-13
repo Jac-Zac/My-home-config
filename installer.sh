f1cb2a2a6bdf9d6e41d29b12057a65a53e735e37
jaczac <jaczac2002@gmail.com>
2020-11-13 12:14:35 +0100

    less in root script

--- installer.sh
+++ installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"i
-brew update && brew upgrade
 echo "brew installed"
 
  # install things to make your rice look like mine
@@ -11,6 +10,17 @@ brew install tty-clock
 brew tap cjbassi/ytop
 brew install ytop
 brew install lsd
+
+# Install ohmyzsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install nerd font 
+cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
+# Make cache directories 
+mkdir .cache
+mkdir .cache/zsh
+mkdir .cache/wget
 
 chmod +x root_script.sh
 echo "run next script as root"
