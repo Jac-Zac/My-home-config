15cc2bbea1d77a924e07ae844038f57ae0a31ce7
Jac-Zac <jacopozac@icloud.com>
2020-12-19 23:19:26 +0100

    How to install homebrew for M1 macbook
    Former-commit-id: 4c9bab74026a2e401ad69507e20881482912cc03

--- README.md
+++ README.md
@@ -61,6 +61,27 @@ cd My-home-config
 ```
 chmod +x installer.sh 
 ```
+
+###### Now you should uncomment the brew installation in installer.sh and install it yourself if you do not have it already. M1 homebrew will be installed in the opt directory 
+```
+cd /opt
+```
+######  Create a directory for Homebrew. This requires root permissions.
+```
+sudo mkdir homebrew
+```
+######  Make us the owner of the directory so that we no longer require root permissions.
+```
+sudo chown -R $(whoami) /opt/homebrew
+```
+######  Download and unzip Homebrew. This command can be found at https://docs.brew.sh/Installation.
+```
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+```
+######  Add the Homebrew bin directory to the PATH. If you don't use zsh, you'll need to do this yourself.
+```
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.config/zsh/.zshrc
+```
 ###### run the first script
 ```
 bash installer.sh 
--- installer.sh
+++ installer.sh
@@ -1,19 +1,6 @@
 !/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"i
-echo "brew installed"
-
- # install things to make your rice look like mine
-brew install zsh
-brew install wget
-brew install neovim
-brew install tty-clock
-brew tap cjbassi/ytop
-brew install ytop
-brew install lsd
-brew install figlet
-
 # Install ohmyzsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make cache directories 
 mkdir .cache
@@ -29,6 +16,23 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 cp -r config $HOME/.config
 cp profile $HOME/.profile
 cp zprofile $HOME/.zprofile
+
+############################ Uncomment this if you use M1 macbook ###################################
+echo "brew installed"																				#
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"   #   
+#####################################################################################################
+
+
+ # install things to make your rice look like mine
+brew install zsh
+brew install wget
+brew install neovim
+brew install tty-clock
+brew tap cjbassi/ytop
+brew install ytop
+brew install lsd
+brew install figlet
+
 
 chmod +x root_script.sh
 
