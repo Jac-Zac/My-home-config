ead41a381b8117a44fe48b74d5364da6b8a81078
Jac-Zac <jacopozac@icloud.com>
2021-02-23 17:06:23 +0100

    New updates for automatic detection

--- installer.sh
+++ installer.sh
@@ -12,26 +12,27 @@ mkdir .cache/zsh
 mkdir .cache/wget
 mv .oh-my-zsh $HOME/.config/oh-my-zsh
 
+cp -r config $HOME/.config
+cp profile $HOME/.profile
+cp zprofile $HOME/.zprofile
+
 # plugins and themes 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.config/oh-my-zsh/}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-cp -r config $HOME/.config
-cp profile $HOME/.profile
-cp zprofile $HOME/.zprofile
 
 
 # tmux configuration with nord theme 
 cd $HOME/.config/tmux/themes 
 git clone https://github.com/arcticicestudio/nord-tmux.git
 
-######################### Uncomment this if you use an Intel macbook ################################
-# echo "brew installed"																				#
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" #   
-# echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.config/zsh/.zshrc								#
-# brew install neovim																				#	
-#####################################################################################################
+
+if [ "$(uname - p)" = "i386" ] ; then
+			echo "brew installed"																				#
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" #   
+			echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.config/zsh/.zshrc								#
+			brew install neovim																				#	
+fi
 
  # install things to make your rice look like mine
 brew install wget
