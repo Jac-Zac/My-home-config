d0c9d65d96695097a1f3af9ba2faadb6c3fa59fd
jaczac <jaczac2002@gmail.com>
2021-02-27 18:40:46 +0100

    New update to the update file

--- update.sh
+++ update.sh
@@ -7,13 +7,18 @@ cp zprofile $HOME/.zprofile
 
 # Updating oh-my-zsh related stuff
 cd $HOME/.config/oh-my-zsh && git pull
-cd $HOME/.config/oh-my-zsh/themes/powerlevel10k && git pull
+cd $HOME/.config/oh-my-zsh/custom/themes/powerlevel10k && git pull
 cd $HOME/.config/oh-my-zsh/custom/plugins/zsh-autosuggestions && git pull
 cd $HOME/.config/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-# install things to make your rice look like mine
-brew update && brew upgrade
+# If MacOS 
+if [ "$(uname)" = "Darwin" ] ; then
+			brew update && brew upgrade
+# If Arch linux
+elif [ "$(uname)" = "Linux" ]; then
+			sudo pacman -Syu
+fi
 
 cd
 
-echo "made by:\n $(figlet JacZac)"
+echo "made by: $(figlet JacZac)"
