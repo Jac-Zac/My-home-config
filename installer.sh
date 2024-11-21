f0fff681222759b79a8784e2a2ca9988ede62a97
Jac-Zac <jacopozac@icloud.com>
2024-11-21 08:46:00 +0100

    Update dependencies

--- installer.sh
+++ installer.sh
@@ -358,6 +358,7 @@ _mainScript_() {
       # Install sketchybar-app-font
       curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
+
       # Start yabai
       yabai --start-service
       skhd --start-service
