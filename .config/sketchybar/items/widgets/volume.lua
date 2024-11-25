55bde4534189a774a868b651fc0f86b462b747ee
Jac-Zac <jacopozac@icloud.com>
2024-11-25 07:38:05 +0100

    New volume config

--- .config/sketchybar/items/widgets/volume.lua
+++ .config/sketchybar/items/widgets/volume.lua
@@ -7,9 +7,7 @@ local popup_width = 250
 local volume_percent = sbar.add("item", "widgets.volume1", {
 	position = "right",
 	icon = { drawing = false },
-	label = {
-		drawing = false,
-	},
+	label = { drawing = false },
 })
 
 local volume_icon = sbar.add("item", "widgets.volume2", {
@@ -20,12 +18,12 @@ local volume_icon = sbar.add("item", "widgets.volume2", {
 		drawing = true,
 		width = 22,
 		align = "center",
+		color = colors.white,
 	},
 })
 
 local volume_bracket = sbar.add("bracket", "widgets.volume.bracket", { volume_icon.name }, {
 	popup = {
-		-- drawing = "toggle",
 		drawing = false,
 		align = "center",
 		blur_radius = settings.popup_blur_radius,
@@ -33,6 +31,7 @@ local volume_bracket = sbar.add("bracket", "widgets.volume.bracket", { volume_ic
 			border_color = colors.popup.border,
 			border_width = settings.popup_border_width,
 			corner_radius = settings.popup_border_radius,
+			color = colors.popup.background,
 		},
 	},
 })
@@ -45,6 +44,7 @@ local volume_slider = sbar.add("slider", popup_width, {
 	background = {
 		height = 80,
 		y_offset = 20,
+		color = colors.popup.background,
 	},
 	slider = {
 		highlight_color = colors.spaces.active,
@@ -56,13 +56,14 @@ local volume_slider = sbar.add("slider", popup_width, {
 		knob = {
 			string = "􀀁",
 			drawing = true,
+			color = colors.white,
 		},
 	},
 	click_script = 'osascript -e "set volume output volume $PERCENTAGE"',
 })
 
-volume_percent:subscribe("volume_change", function(env)
-	local volume = tonumber(env.INFO)
+local function update_volume_display(volume)
+	volume = tonumber(volume)
 	local icon = icons.volume._0
 	local color = colors.white
 	if volume > 60 then
@@ -77,15 +78,61 @@ volume_percent:subscribe("volume_change", function(env)
 		icon = icons.volume._0
 		color = colors.grey
 	end
-	local lead = ""
-	if volume < 10 then
-		lead = "0"
-	end
-	volume_icon:set({ icon = { string = icon, color = color } })
-	volume_slider:set({ slider = { percentage = volume } })
-end)
 
-local current_audio_device = "None"
+	volume_icon:set({
+		icon = {
+			string = icon,
+			color = color,
+			drawing = true,
+		},
+	})
+	volume_slider:set({
+		slider = {
+			percentage = volume,
+			drawing = true,
+		},
+	})
+end
+
+local function create_device_item(device, current, counter)
+	local is_current = current == device
+	return {
+		position = "popup." .. volume_bracket.name,
+		padding_right = settings.popup_padding,
+		padding_left = settings.popup_padding,
+		y_offset = 8,
+		width = popup_width,
+		background = {
+			color = colors.popup.background,
+			drawing = true,
+		},
+		label = {
+			string = device,
+			color = is_current and colors.white or colors.grey,
+			drawing = true,
+		},
+		click_script = string.format(
+			'SwitchAudioSource -s "%s" && sketchybar --set /volume.device\\.*/ label.color=0xff999999 --set $NAME label.color=0xffffffff',
+			device
+		),
+	}
+end
+
+local function update_device_list()
+	sbar.exec("SwitchAudioSource -t output -c", function(result)
+		local current_device = result:sub(1, -2)
+		sbar.exec("SwitchAudioSource -a -t output", function(available)
+			-- Remove existing device items
+			sbar.remove("/volume.device\\.*/")
+
+			local counter = 0
+			for device in string.gmatch(available, "[^\r\n]+") do
+				sbar.add("item", "volume.device." .. counter, create_device_item(device, current_device, counter))
+				counter = counter + 1
+			end
+		end)
+	end)
+end
 
 local function volume_toggle_details(env)
 	if env.BUTTON == "right" then
@@ -96,44 +143,10 @@ local function volume_toggle_details(env)
 	local current_drawing = volume_bracket:query().popup.drawing
 	local should_draw = current_drawing == "off"
 
-	-- Toggle popup visibility
 	volume_bracket:set({ popup = { drawing = should_draw } })
 
-	-- If opening the popup, refresh the details
 	if should_draw then
-		sbar.exec("SwitchAudioSource -t output -c", function(result)
-			current_audio_device = result:sub(1, -2)
-			sbar.exec("SwitchAudioSource -a -t output", function(available)
-				current = current_audio_device
-				local color = colors.grey
-				local counter = 0
-
-				-- Remove any existing device items first
-				sbar.remove("/volume.device\\.*/")
-
-				for device in string.gmatch(available, "[^\r\n]+") do
-					local color = colors.grey
-					if current == device then
-						color = colors.white
-					end
-					sbar.add("item", "volume.device." .. counter, {
-						position = "popup." .. volume_bracket.name,
-						padding_right = settings.popup_padding,
-						padding_left = settings.popup_padding,
-						y_offset = 8,
-						width = popup_width,
-						label = { string = device, color = color },
-						click_script = 'SwitchAudioSource -s "'
-								.. device
-								.. '" && sketchybar --set /volume.device\\.*/ label.color='
-								.. colors.grey
-								.. " --set $NAME label.color="
-								.. colors.white,
-					})
-					counter = counter + 1
-				end
-			end)
-		end)
+		update_device_list()
 	end
 end
 
@@ -142,10 +155,25 @@ local function volume_scroll(env)
 	sbar.exec('osascript -e "set volume output volume (output volume of (get volume settings) + ' .. delta .. ')"')
 end
 
+volume_percent:subscribe("volume_change", function(env)
+	update_volume_display(env.INFO)
+end)
+
+sbar.exec('osascript -e "output volume of (get volume settings)"', function(result)
+	update_volume_display(result)
+end)
+
 volume_icon:subscribe("mouse.clicked", volume_toggle_details)
 volume_icon:subscribe("mouse.scrolled", volume_scroll)
 volume_percent:subscribe("mouse.clicked", volume_toggle_details)
 volume_percent:subscribe("mouse.scrolled", volume_scroll)
 
--- Remove the mouse.exited.global event to prevent popup from automatically closing
--- volume_percent:subscribe("mouse.exited.global", volume_collapse_details)
+-- Add a new subscription to close the popup when clicking outside
+sbar.subscribe("mouse.clicked.global", function(env)
+	if env.BUTTON == "left" and not is_click_inside_popup(env) then
+		local is_popup_visible = volume_bracket:query().popup.drawing == "on"
+		if is_popup_visible then
+			volume_collapse_details()
+		end
+	end
+end)
