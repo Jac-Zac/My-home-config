de276eecab7afc1f5c01dff8d68ce541206a75b1
Jac-Zac <jacopozac@icloud.com>
2025-03-24 18:28:35 +0100

    Adding new configs

---
+++ HPC_config/.zprofile
@@ -0,0 +1,4 @@
+export ZDOTDIR="${XDG_CONFIG_HOME:-$HOME/.config}/zsh"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export ZSH_COMPDUMP="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/.zcompdump-${ZSH_VERSION}"
---
+++ HPC_config/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat .zprofile > ~/.zprofile
+
+# Set config
+mkdir ~/.config
+
+# Create cache dir and zsh history
+mkdir ~/.cache
+mkdir ~/.cache/zsh
+
+# Set shell dir
+mkdir ~/.config/shell/
+
+# Fast synatx hilighting
+git clone https://github.com/zdharma/fast-syntax-highlighting ~/.config/shell/fast-syntax-highlighting
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/shell/zsh-autosuggestions/
